e2d88f7496729aa7ca2868b4f26664e6b7b9dcbd
Vladyslava Zhmurkevych <vladyslava_zhmurkevych@epam.com>
2019-09-03 18:44:50 +0300

    WIP: pagination, input clear after adding elemen

--- toDoList/src/app/modules/home/field/field.component.ts
+++ toDoList/src/app/modules/home/field/field.component.ts
@@ -8,12 +8,29 @@ import {State, Store} from "../store/store";
 })
 export class FieldComponent implements OnInit {
   todoList: string[];
+  page: number = 0;
   constructor(private store: Store) { }
 
   ngOnInit() {
     this.store.state$.subscribe((state: State) => {
       this.todoList = state.toDoList;
+      this.page = Math.floor(this.todoList.length / 5);
+
     })
+
   }
 
+  nextPage() {
+    if(this.todoList.length > (this.page + 5)){
+      this.page++;
+    }
+
+  }
+
+  prevPage(){
+    if(this.page !== 0) {
+      this.page--;
+    }
+
+  }
 }
